40a440307880f27f829f72a1a80b42926d7fd043
Darryl <darrylf08@gmail.com>
2019-10-10 08:29:17 +0000

    Corrected output variables

--- awsproject/outputs.tf
+++ awsproject/outputs.tf
@@ -5,8 +5,8 @@
 #####################################################################
 
 output "aws_server_ip_address" {
-  value =  "$(aws_instance.aws_server.public_ip)"
+  value =  "${aws_instance.aws_server.public_ip}"
   }
 output "aws_server_public_dns" {
-  value =  "$(aws_instance.aws_server.public_dns)"
+  value =  "${aws_instance.aws_server.public_dns}"
   }
