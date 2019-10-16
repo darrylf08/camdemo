3fc44de4dbb39414219ea26047acd089af6cf38a
Darryl <darrylf08@gmail.com>
2019-10-16 12:41:24 +0000

    Changed back to public DNS name

--- awsproject/outputs.tf
+++ awsproject/outputs.tf
@@ -7,6 +7,6 @@
 output "aws_server_ip_address" {
   value =  "${aws_instance.aws_server.public_ip}"
   }
-output "aws_server_login" {
-  value =  "<a href=ssh://ec-user@${aws_instance.aws_server.public_dns}/c:\\Temp\\SSH_URL\\key-pair-1.ppk>Click here</a> "
+output "aws_server_public_dns" {
+  value =  "${aws_instance.aws_server.public_dns}"
   }
